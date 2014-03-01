7f1e2fb7edb92010734c560f5e8f1671d4102316
loic lefloch <l.lefloch.91@gmail.com>
2014-03-01 15:03:35 +0100

    maj install emacs conf

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,4 +1,11 @@
+echo "Installing emacs"
+
 cp -r .emacs.d/ ~/
 cp -r .emacs ~/
 cp -r .myemacs ~/
-cp .std* ~/
+
+echo "Installing header. Choose No if already exist :)"
+cp -i .std* ~/
+mkdir ~/.emacs_backups
+
+echo "Please change your name and login on .std-comment.el";
